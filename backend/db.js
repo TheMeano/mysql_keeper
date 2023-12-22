a7210a3d630e9b54116416ec79b7b0310d9d0c0b
TheMeano <smeana@gmx.net>
2023-12-22 19:27:43 +0100

    DB version

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,6 @@
 import mysql from "mysql";
 import 'dotenv/config'
 
- export const db = mysql.createConnection({
-    host: "localhost",
-    user: "rootbasti",
-    password: process.env.DB_KEY,
-    database: "keeper"
- })
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+
+ export const db = mysql.createConnection(urlDB) 
